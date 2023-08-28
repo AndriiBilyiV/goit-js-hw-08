1d795612fff3cb9f4ebc0cccfb4a6073f34567b7
AndriiBilyiV <andrey.dobraf@gmail.com>
2023-08-29 01:40:28 +0300

    done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,31 @@
+import throttle  from "lodash/throttle";
 
+const form = document.querySelector('.feedback-form');
+const email = form.querySelector('input[name="email"]');
+const message = form.querySelector('textarea[name="message"]');
+
+try {
+    const lastValues = JSON.parse(localStorage.getItem('feedback-form-state'))
+    email.value = lastValues.email;
+    message.value = lastValues.message;
+} catch (err) {
+    console.log(err)
+}
+
+form.addEventListener('input', throttle(() => {
+    
+    console.log(email);
+    const values = {
+        email: email.value,
+        message: message.value
+    }
+    localStorage.setItem('feedback-form-state', JSON.stringify(values))
+}, 500)
+)
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(localStorage.getItem('feedback-form-state'));
+    localStorage.removeItem('feedback-form-state');
+    email.value = '';
+    message.value = '';
+})
