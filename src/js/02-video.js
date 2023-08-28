c190ff4706a09f2a3febbed4c32f873f8eb9103b
AndriiBilyiV <andrey.dobraf@gmail.com>
2023-08-29 01:00:14 +0300

    localStorage

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash/throttle';
+console.log(throttle)
 
+const iframe = document.querySelector('iframe');
+const myPlayer = new Player(iframe);
+
+myPlayer.on('timeupdate',  throttle((sec) => {
+    localStorage.setItem('videoplayer-current-time', sec.seconds);
+    console.log(localStorage.getItem('videoplayer-current-time'))
+    }, 1000)
+)
